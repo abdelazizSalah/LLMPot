f17ce42ed57ef77ae227eb87416260d478bfa11d
Abdelaziz Salah <abdelaziz132001@gmail.com>
2026-01-28 11:03:24 +0100

    creating plotting script for 5000 c0

---
+++ src/plots/mbtcp/bca_rva_wdt_5000_attack_c0.py
@@ -0,0 +1,8 @@
+from src.plots.from_csv import NATURE, Plots
+
+
+plot = Plots("wdt_attack1_c0_5000.json", "20260127T1417")
+colors = {dataset.functions_str(): NATURE[i] for i, dataset in enumerate(plot.finetuner.datasets)}
+labels = [dataset.functions_str() for dataset in plot.finetuner.datasets]
+plot.accuracy_per_epoch(colors, labels)
+plot.loss_per_epoch(colors, labels)
