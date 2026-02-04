f2eb33797a2544aca5a57e0b4094fd2b4fba34bb
abdelazizSalah <abdelaziz132001@gmail.com>
2026-02-04 17:41:37 +0100

    getting results

---
+++ src/plots/mbtcp/bca_rva_wdt_wdt_normal_c1_200.py
@@ -0,0 +1,7 @@
+from src.plots.from_csv import NATURE, Plots
+
+plot = Plots("wdt_normal_c1_200.json", "20260203T1605")
+colors = {dataset.functions_str(): NATURE[i] for i, dataset in enumerate(plot.finetuner.datasets)}
+labels = [dataset.functions_str() for dataset in plot.finetuner.datasets]
+plot.accuracy_per_epoch(colors, labels)
+plot.loss_per_epoch(colors, labels)
